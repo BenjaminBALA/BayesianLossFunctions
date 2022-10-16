08366c3ef17193748678f738a97d8ee6c164135e
BenjaminBALA <49543217+BenjaminBALA@users.noreply.github.com>
2022-10-16 08:34:31 -0700

    Update BayesianLossFunctions.r

--- BayesianLossFunctions.r
+++ BayesianLossFunctions.r
@@ -7,10 +7,10 @@ for (i in 1:a ){
   loss_optimal <- (15/3 - y)^2
 }
 mean(loss_optimal)
-min <- 15/3^2
-mean(loss_optimal)- min
+min <- 15/3^2 #the minimum loss occurs at alpha/beta^2 which is also equal to the variance of a beta
+mean(loss_optimal)- min #The difference between these two should be approximately 0 on average
 
-q=.75
+q=.75 #Hyperparameter that is fixed to take a mixure of the two different distributions
 if (y[a] > a) {
   loss <- q*(y[a]-a)
 } else {
@@ -33,15 +33,13 @@ if (y[a] != a){
 } else {
   loss <- c(loss, 0)
 }
-y
-loss
+#This code uses the identity fuction in order to check to see if the Poisson distribution is different that the value a and returns a 1 if the values are the same and a 0 if the values are different
 mean(loss)
-a <- floor(5.3)
+a <- floor(5.3) #This takes the floor of the Poisson parameter and checks to see if it is different than the value a
 y <- rpois(20, a)
 if (y[a] != a){
   newvalue = 1
   loss <- c(loss, newvalue)  
 }
-y
-loss
+
 mean(loss)
