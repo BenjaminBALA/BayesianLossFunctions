971eb7ab4f4a5bc4a5fe00ac55c69e1f575e61a8
BenjaminBALA <49543217+BenjaminBALA@users.noreply.github.com>
2022-10-17 09:02:11 -0700

    Update BayesianLossFunctions.r

--- BayesianLossFunctions.r
+++ BayesianLossFunctions.r
@@ -1,12 +1,14 @@
-set.seed(123)
-a <- 20
+set.seed(123) #Setting the seed is necessary when using random data for simulating the rgamma function
+a <- 20 #This is the action variable that ranges from 1 to 20
 
+#This is a loop to calculate the square loss of the gamma function with various action amounts
 for (i in 1:a ){
   y <- rgamma(a, 15, 3)
   loss <- (a-y[a])^2
   loss_optimal <- (15/3 - y)^2
 }
-mean(loss_optimal)
+
+mean(loss_optimal) #Taking the average loss with the optimal action which is the mean alpha/beta
 min <- 15/3^2 #the minimum loss occurs at alpha/beta^2 which is also equal to the variance of a beta
 mean(loss_optimal)- min #The difference between these two should be approximately 0 on average
 
@@ -26,7 +28,7 @@ if (y[a] > x) {
 mean(loss2)
 #The optimal action is when q is the inverse of the CDF for Y
 a <- 20
-y <- rpois(a, 5.3)
+y <- rpois(a, 5.3) #Simulate a Poisson with lambda eqaul to 5.3
 if (y[a] != a){
   newvalue = 1
   loss <- c(loss, newvalue)  
